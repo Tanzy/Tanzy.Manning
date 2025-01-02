e2472fe86227556f2781815477ee5bb2ac74b8c6
Ken Lea <ken.lea@towerswatson.com>
2025-01-02 09:21:31 +0000

    updates

--- readFile.py
+++ readFile.py
@@ -1,14 +1,26 @@
 import json
 import os
 
+
+# Get data function
+def getFileData(files):
+
+    filesData = []
+
+    for file in files:
+        if file.endswith('.json'):
+            with open(os.path.join('sampleFiles', file)) as f:
+                filesData.append(json.load(f))
+
+    return filesData
+
+
+
 # get list of files in the sampleFiles folder
 files = os.listdir('sampleFiles')
 
-for file in files:
-    if file.endswith('.json'):
-        with open(os.path.join('sampleFiles', file)) as f:
-            data = json.load(f)
-            print(f"Contents of {file}:")
-            print(data)
+highlights = getFileData(files)
 
-
+# list data that is contained in fileData
+for data in highlights:
+    print(data)
