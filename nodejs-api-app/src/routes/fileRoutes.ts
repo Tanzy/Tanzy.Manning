d5dacee4e66e23443f5d31d7db5459539a8d3f75
Ken Lea <git@kenlea.dev>
2025-02-26 22:13:11 +0000

    remove unused files and dependencies from the project

--- nodejs-api-app/src/routes/fileRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { processJson } from '../controllers/fileController';
-
-const router = express.Router();
-
-router.post('/upload', processJson);
-
-export default router;
